d5e827dcd195fb7ac36e36435dedfebfd9b9b866
MyingJason <myingon@gmail.com>
2014-05-22 18:10:28 +1000

    server-client

--- pacman/src/main/java/com/pacman/pacmanserver/ClientObject.java
+++ pacman/src/main/java/com/pacman/pacmanserver/ClientObject.java
@@ -2,6 +2,8 @@ package com.pacman.pacmanserver;
 
 import java.io.Serializable;
 
+import com.pacman.model.Pacman;
+
 public class ClientObject implements Serializable {
 
 	/**
@@ -9,10 +11,12 @@ public class ClientObject implements Serializable {
 	 */
 	private static final long serialVersionUID = 4895280901013246381L;
 	String value;
+	Pacman pacman;
 
-	public ClientObject(String value) {
+	public ClientObject(String value,Pacman pacman) {
 		super();
 		this.value = value;
+		this.pacman = pacman;
 	}
 	 
 
