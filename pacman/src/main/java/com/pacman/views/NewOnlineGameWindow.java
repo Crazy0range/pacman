a65db28ab85d4ad47d6bf64ee10a725a8307609a
Crazy0range <liusiyuan_lsy@msn.com>
2014-05-26 01:43:50 +1000

    added connect to server button

--- pacman/src/main/java/com/pacman/views/NewOnlineGameWindow.java
+++ pacman/src/main/java/com/pacman/views/NewOnlineGameWindow.java
@@ -24,6 +24,7 @@ public class NewOnlineGameWindow extends JFrame {
 	private JButton btnCancel;
 	private JButton btnStartElection;
 	private JTextPane textPane;
+	private JButton btnConnectToServer;
 
 	/**
 	 * Used for creating a new WhiteBord window.
@@ -31,7 +32,7 @@ public class NewOnlineGameWindow extends JFrame {
 	public NewOnlineGameWindow(UserProfile uPrf) {
 		setTitle("New Online Game");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 400, 300);
+		setBounds(100, 100, 400, 360);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -54,7 +55,7 @@ public class NewOnlineGameWindow extends JFrame {
 				// dispose();
 			}
 		});
-		btnStart.setBounds(136, 217, 128, 40);
+		btnStart.setBounds(217, 217, 128, 40);
 		contentPane.add(btnStart);
 		btnStart.setForeground(Color.BLACK);
 		btnStart.setFont(new Font("Segoe UI Light", Font.PLAIN, 18));
@@ -62,7 +63,7 @@ public class NewOnlineGameWindow extends JFrame {
 		btnStart.setEnabled(false);
 
 		btnCancel = new JButton("Cancel");
-		btnCancel.setBounds(268, 217, 128, 40);
+		btnCancel.setBounds(217, 269, 128, 40);
 		contentPane.add(btnCancel);
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -82,7 +83,7 @@ public class NewOnlineGameWindow extends JFrame {
 		btnStartElection.setForeground(Color.BLACK);
 		btnStartElection.setFont(new Font("Dialog", Font.PLAIN, 18));
 		btnStartElection.setBackground(new Color(40, 120, 255));
-		btnStartElection.setBounds(4, 217, 128, 40);
+		btnStartElection.setBounds(43, 217, 128, 40);
 		contentPane.add(btnStartElection);
 
 		textPane = new JTextPane();
@@ -93,6 +94,18 @@ public class NewOnlineGameWindow extends JFrame {
 		textPane.setBounds(16, 16, 366, 189);
 		textPane.setText("Waiting for leader...\n");
 		contentPane.add(textPane);
+		
+		btnConnectToServer = new JButton("Connect");
+		btnConnectToServer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO connect to server methods here
+			}
+		});
+		btnConnectToServer.setForeground(Color.BLACK);
+		btnConnectToServer.setFont(new Font("Dialog", Font.PLAIN, 18));
+		btnConnectToServer.setBackground(new Color(40, 120, 255));
+		btnConnectToServer.setBounds(43, 269, 128, 40);
+		contentPane.add(btnConnectToServer);
 
 	}
 
